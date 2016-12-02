b7603ede913643c9c41538b88fe09b83b7a3037b
chichkanov <agchichkanov@gmail.com>
2016-12-02 13:02:36 +0300

    Main methods

--- KR2module/KR2module/Program.cs
+++ KR2module/KR2module/Program.cs
@@ -10,6 +10,22 @@ namespace KR2module
     {
         static void Main(string[] args)
         {
+            Console.Title = "Chichkanov";
+
+            do
+            {
+                Console.Clear();
+
+                ShowProgramNotification();
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
+
+        /// <summary>
+        /// Уведомление пользователя о работе программы
+        /// </summary>
+        private static void ShowProgramNotification()
+        {
+            Console.WriteLine("Нажмите любую клавишу для повторного решения.\nНажмите ESC для выхода.");
         }
     }
 }
